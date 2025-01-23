37bfd664c27fe887871c2321828ec4513ac76acf
Motati Nikhilvardhanreddy <motatinikhilvardhanreddy@gmail.com>
2025-01-23 12:23:42 +0530

    r1

---
+++ add.py
@@ -0,0 +1,10 @@
+#This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+#Add two numbers 
+sum = num1+num2[D[D[D[D[D[C[C[C[C[D[D[D[D [C[ + num2
+#Display the sum
+print('The sum of {0} and {1}
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C is {2}'.format(num1,num2, sum)[D[D[D[D[D[D[D[D[D[D [D     [D[D[D[D[D[Cnum2,[C[C[ sum)
+cat add.py
